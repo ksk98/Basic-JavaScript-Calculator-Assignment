64cbd44fe913d3274d834cfd26f2527977c6f8c2
Krzysztof Krawczyk <krzysztofskrawczyk@gmail.com>
2022-11-11 15:38:21 +0100

    Temat 5 progress

--- Temat 5/api/index.js
+++ Temat 5/api/index.js
@@ -7,21 +7,45 @@ module.exports = {
 };
 
 const testData = {
-    id: "0123456789abcd",
-    title: "Testowy obrazek",
-    description: "Opis do obrazka",
-    date: "2017-11-09T10:20:00.214Z",
-    path: "/library/images/",
-    size: 1024
+    "0123456789abcd": {
+        id: "0123456789abcd",
+        title: "Testowy obrazek",
+        description: "Opis do obrazka",
+        date: "2017-11-09T10:20:00.214Z",
+        path: "/library/images/",
+        size: 1024
+    }
 };
 
+const { v4: uuidv4 } = require('uuid');
+
 function listImages(req, res, next) {
-    console.log("hehe")
-    res.json();
+    const out = []
+    for (let elem in testData) {
+        out.push({
+            id: testData[elem].id,
+            title: testData[elem].title,
+            path: testData[elem].path
+        })
+    }
+
+    res.json({
+        images: out
+    });
 }
 
 function createImage(req, res, next) {
-    res.json();
+    const image = {
+        id: uuidv4(),
+        title: req.body.name,
+        description: req.body.description,
+        date: new Date().toISOString(),
+        path: "/library/images/",
+        size: (new TextEncoder().encode(req.body.upFile)).length
+    }
+
+    testData[image.id] = image
+    res.json(image);
 }
 
 function readImage(req, res, next) {
--- Temat 5/app.js
+++ Temat 5/app.js
@@ -4,6 +4,9 @@ const { connector } = require('swagger-routes-express')
 const swaggerUi = require('swagger-ui-express');
 const api = require('./api')
 
+const bodyParser = require('body-parser')
+const jsonParser = bodyParser.json()
+
 const makeApp = () => {
     const apiDefinition = YAML.load('swagger.yaml') // load the api as json
     const connect = connector(api, apiDefinition) // make the connector
@@ -11,6 +14,7 @@ const makeApp = () => {
 
     // do any other app stuff, such as wire in passport, use cors etc
     app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(apiDefinition));
+    app.use(bodyParser.json())
     app.listen(3000, () => console.log("App is listening on 3000"))
 
     connect(app) // attach the routes
